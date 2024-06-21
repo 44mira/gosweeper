fcc255ab4d4587cf5fc24cd90a0ee6888f75ee08
44mira <legolaslada@gmail.com>
2024-06-21 13:57:41 +0800

    feat: add colors

--- gosweeper.go
+++ gosweeper.go
@@ -1,7 +1,27 @@
 package main
 
+const (
+	Reset         = "\033[0m"
+	Black         = "\033[30m"
+	Red           = "\033[31m"
+	Green         = "\033[32m"
+	Yellow        = "\033[33m"
+	Blue          = "\033[34m"
+	Magenta       = "\033[35m"
+	Cyan          = "\033[36m"
+	Gray          = "\033[37m"
+	White         = "\033[97m"
+	BrightRed     = "\033[91m"
+	BrightGreen   = "\033[92m"
+	BrightYellow  = "\033[93m"
+	BrightBlue    = "\033[94m"
+	BrightMagenta = "\033[95m"
+	BrightCyan    = "\033[96m"
+	BrightWhite   = "\033[97m"
+)
+
 func main() {
-	game := Initialize(10, 10, 50)
+	game := Initialize(10, 10, 25)
 
 	game.Display()
 }
